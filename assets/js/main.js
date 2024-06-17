f5a197c0b34559330d5d44aab2df76c796eff852
микемка <mikemka@vk.com>
2024-06-17 23:33:19 +0500

    add TG init js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 function sendMessage() {
     alert(1);
 }
+
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.setText("Кнопка");
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+// tg.MainButton.hide();
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData('Нажата основная кнопка');
+});
