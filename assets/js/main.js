23316263319c06706223f4b2e67004078a54360b
микемка <mikemka@vk.com>
2024-06-17 23:59:15 +0500

    FIX: add tg js file; FEAT: add loading old msgs from js, put messages to chat from input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,52 @@
-function sendMessage() {
-    alert(1);
+function addMessage(text, sendedFromUser, isNewMessage) {
+    const newMessageBox = document.createElement("div");
+    newMessageBox.classList.add('message');
+    
+    if (sendedFromUser) {
+        newMessageBox.classList.add('out');
+    } else {
+        newMessageBox.classList.add('in');
+    }
+    
+    const newMessage = document.createElement("div");
+    const newContent = document.createTextNode(text);
+    newMessage.appendChild(newContent);
+    
+    newMessageBox.appendChild(newMessage);
+    
+    const messagesDiv = document.getElementById('messages');
+    if (isNewMessage) {
+        messagesDiv.insertBefore(newMessageBox, messagesDiv.firstChild);
+    } else {
+        messagesDiv.appendChild(newMessageBox);
+    }
 }
+
+function sendMessage() {
+    const input = document.getElementById('message-input');
+    const text = input.value;
+    if (!text) {
+        return
+    }
+    input.value = '';
+    addMessage(text, true, true);
+}
+
+function loadOldMessages() {
+    const oldMessages = [['тест соо 1', true], ['тест отв 1', false]];
+    for (const message of oldMessages.reverse()) {
+        addMessage(message[0], message[1], false)
+    }
+}
+
+window.onload = loadOldMessages;
 
 let tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.setText("Кнопка");
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
-// tg.MainButton.hide();
+tg.MainButton.hide();
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData('Нажата основная кнопка');
 });
