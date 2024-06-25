8d162942412d266e925f111f51a1f058f5293ac7
микемка <mikemka@vk.com>
2024-06-25 14:58:51 +0500

    аааааааАААААААА помогите

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,9 +90,13 @@ function showMenu() {
     if (menu) {
         document.getElementById('message-input').classList.remove('d-none');
         document.getElementById('menu-container').classList.add('d-none');
+        document.getElementById('showMenuButton').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path d="M3 6H21M3 12H21M3 18H21" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+        document.getElementById('sendMessageButton').classList.remove('d-none');
     } else {
         document.getElementById('message-input').classList.add('d-none');
         document.getElementById('menu-container').classList.remove('d-none');
+        document.getElementById('showMenuButton').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path d="M6 6L19 18M19 6L6 18" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+        document.getElementById('sendMessageButton').classList.add('d-none');
     }
     menu = !menu;
 }
